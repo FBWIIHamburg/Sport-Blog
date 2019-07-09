179b51a18857aa3f96c8c5321176c549cfd5e490
mahasensio <98mahakahlah@gmail.com>
2019-07-09 17:23:56 +0200

    last update 09.07.19

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const usersRouter = require('./src/routes/users');
 const registerRouter = require('./src/routes/register');
 const siginRouter = require('./src/routes/signin');
 const verificationRouter=require('./src/controllers/verification')
-
+const passRouter = require('./src/routes/changepassowrd');
 const app = express();
 
 // view engine setup
@@ -31,6 +31,30 @@ res.send(req.body)
 app.use('/register', registerRouter);
 app.use('/signin', siginRouter);
 app.use('/verification', verificationRouter);
+app.use('/passowrd', passRouter);
+
+
+app.use('/loged', logedRouter);
+app.use('/loged/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 app.use('/loged', logedRouter);
 app.use('/loged/users', usersRouter);
